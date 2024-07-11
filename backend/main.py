1f333de488ce90316dd0bbb77554da51e8b62dc9
filipeorfao <filipeaorfao@gmail.com>
2024-07-11 15:15:30 +0100

    add frontend page

--- backend/main.py
+++ backend/main.py
@@ -1,14 +1,38 @@
-from fastapi import FastAPI, HTTPException, Depends
-from pydantic import BaseModel
 import uvicorn
+from pydantic import BaseModel
+
+from fastapi import FastAPI, HTTPException, Depends
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+from fastapi import FastAPI, Response, Request
+
+from utils import get_stock_info
 
 
 app = FastAPI()
+
+# app.mount("/frontend", StaticFiles(directory="../frontend"), name="frontend")
+
+template_obj = Jinja2Templates(directory="../frontend")
 
 
 @app.get("/api/health")
 def get_health():
     return {"message": "Ok"}
+
+
+# http://127.0.0.1:8000/api/thing?thing=35325
+@app.get("/api/thing")
+async def get_health(request: Request):
+    company_info = get_stock_info("msft")
+    # if not company_info:
+    #     catch if shit goes wrong
+
+    # return {**get_stock_info(thing)}
+    return template_obj.TemplateResponse(
+        name="index.html",
+        context={"request": request, "info": company_info},
+    )
 
 
 # uvicorn main:app --reload
--- backend/utils.py
+++ backend/utils.py
@@ -1,73 +1,35 @@
-import requests
-from bs4 import BeautifulSoup
-import datetime
-import re
+import yfinance as yf
 
 
-def get_yesterday_date():
-    today = datetime.datetime.now()
-    yesterday = today - datetime.timedelta(days=1)
-    yesterday_str = yesterday.strftime("%Y-%m-%d")
-    return yesterday_str
+def get_stock_info(ticker):
+    stock_info = yf.Ticker(ticker)
+    stock_info_wanted = {
+        i: (stock_info.info[i] if i in stock_info.info else i)
+        for i in [
+            "marketCap",
+            "shortName",
+            "longName",
+            "trailingPE",
+            "forwardPE",
+            "priceToSalesTrailing12Months",
+            "evToEbitda",
+            "priceToBook",
+            "free cash flow yield",
+            "freeCashflow",
+            "poitroski score",
+            "quality rating",
+            "profitMargins",
+            "operatingMargins",
+            "quarterly earnings",
+            "quarterly revenue",
+            "totalCash",
+            "totalDebt",
+            "netIncomeToCommon",
+            "dividendYield",
+            "payoutRatio",
+            "exDividendDate",
+            "payout date",
+        ]
+    }
 
-
-def get_stock_info(stock, date):
-    headers = {"User-Agent": "Mozilla/5.0"}
-    backend_url = f"https://sistemasweb.b3.com.br/PlantaoNoticias/Noticias/ListarTitulosNoticias?agencia=18&palavra={stock}&dataInicial={date}&dataFinal={date}"
-
-    link_pattern = r"https://.*?ID=[0-9]+&flnk"
-
-    res = requests.get(backend_url, headers=headers)
-    res.json()
-
-    for i in res.json():
-        pdf_res = requests.get(
-            f'https://sistemasweb.b3.com.br/PlantaoNoticias/Noticias/Detail?idNoticia={i["NwsMsg"]["id"]}&agencia={i["NwsMsg"]["IdAgencia"]}&dataNoticia={i["NwsMsg"]["dateTime"].replace(" ", "%20")}',
-            headers=headers,
-        )
-        res_html = BeautifulSoup(pdf_res.text)
-        res_html.find_all("pre")
-        match = re.search(link_pattern, res_html.find_all("pre")[0].text.strip())
-        if match:
-            print(i["NwsMsg"]["headline"])
-            print(match.group(0))
-
-
-def get_news_link():
-    pass
-
-
-def get_pdf_link():
-    pass
-
-
-def main():
-    stocks = [
-        "bbdc",
-        "taee",
-        "unipar",
-        "wizc",
-        "hapv",
-        "shul",
-        "aure",
-        "tris",
-        "tasa",
-        "klbn",
-        "jhsf",
-        "itub",
-        "sapr",
-        "cmig",
-        "eletrobras",
-        "bees",
-        "bees",
-        "bbas",
-        "rani",
-        "csmg",
-        "petrobras",
-        "irbr",
-        "xpbr",
-        "fesa",
-    ]
-    date = get_yesterday_date()
-    for stock in stocks:
-        get_stock_info(stock, date)
+    return stock_info_wanted
